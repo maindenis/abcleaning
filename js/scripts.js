5ab6880973047b934bc8d3803adf3982ccd6e5ba
maindenis <165322876+maindenis@users.noreply.github.com>
2024-12-01 17:31:22 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,29 @@ d = document,
 e = d.documentElement,
 g = d.getElementsByTagName('body')[0],
 bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+var sliderIsLive;
+function initialSlider() {
+    if($('.packages_thumbs').length > 0) {
+        if (window.innerWidth <= 700) {
+            $('.packages_thumbs').slick({
+                dots: false,
+                arrows: true,
+                autoplaySpeed: 4000,
+                speed: 2000,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: true
+            });
+            sliderIsLive = true;
+        }
+        else {
+            if (sliderIsLive) {
+                $('.packages_thumbs').slick('unslick');
+                sliderIsLive = false;
+            }
+        }
+    }
+}
 
 $(window).resize(function() {
 
@@ -12,7 +35,13 @@ $(document).scroll(function() {
 
 });
 
+window.addEventListener("resize", function() {
+initialSlider();
+});
+
 $(document).ready(function() {
+
+    initialSlider();
 
     if( $(".rev_slider").length > 0 ) {
         $(".rev_slider").not(".slick-initialized").slick({
@@ -52,46 +81,60 @@ $(document).ready(function() {
 
     // --------------
 
-    $(".respmenubtn").click(function(e) {
+    $(".respBtn").click(function(e) {
       e.preventDefault();
-      if( $("#resp_nav").is(":hidden") ) {
-          $("#resp_nav").fadeIn(300);
+      if( $("#respNav").is(":hidden") ) {
+          $("#respNav").fadeIn(300);
+          $(".res_bg").fadeIn(300);
           $(this).addClass("active");
+          $(".closeNav").addClass("active");
       } else {
-          $("#resp_nav").fadeOut(300);
+          $("#respNav").fadeOut(300);
+          $(".res_bg").fadeOut(300);
           $(this).removeClass("active");
+          $(".closeNav").removeClass("active");
       }
     });
     
     $(this).keydown(function(eventObject){
         if (eventObject.which == 27 &&
-            $("#resp_nav").is(":visible") &&
-            bodyWidth <= 767) {
-                $("#resp_nav").fadeOut(300);
-                $(".respmenubtn").removeClass("active");
+            $("#respNav").is(":visible") &&
+            bodyWidth <= 900) {
+                $("#respNav").fadeOut(300);
+                $(".respBtn").removeClass("active");
+                $(".closeNav").removeClass("active");
+                $(".res_bg").fadeOut(300);
         }
+    });
+
+    $(".res_bg, .closeNav").click(function(e) {
+        e.preventDefault();
+        $("#respNav").fadeOut(300);
+        $(".respBtn").removeClass("active");
+        $(".closeNav").removeClass("active");
+        $(".res_bg").fadeOut(300);
     });
 
     // --------------
 
-    var mapZoom;
-    var lat;
-    var long;
-    mapZoom = $("#map").attr("data-zoom");
-    lat = $("#map").attr("data-lat");
-    long = $("#map").attr("data-long");
-    ymaps.ready(function () {        
-        var myMap = new ymaps.Map('map', {
-            center: [long, lat],
-            zoom: mapZoom
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
-        myPlacemark1 = new ymaps.Placemark([long, lat], {
-            hintContent: ''
-        }, {
-        });
-        myMap.geoObjects.add(myPlacemark1);        
-    });
+    // var mapZoom;
+    // var lat;
+    // var long;
+    // mapZoom = $("#map").attr("data-zoom");
+    // lat = $("#map").attr("data-lat");
+    // long = $("#map").attr("data-long");
+    // ymaps.ready(function () {        
+    //     var myMap = new ymaps.Map('map', {
+    //         center: [long, lat],
+    //         zoom: mapZoom
+    //     }, {
+    //         searchControlProvider: 'yandex#search'
+    //     });
+    //     myPlacemark1 = new ymaps.Placemark([long, lat], {
+    //         hintContent: ''
+    //     }, {
+    //     });
+    //     myMap.geoObjects.add(myPlacemark1);        
+    // });
 
 });
